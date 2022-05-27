5f5b5fe6314b44e0bd13187463611ab8e0cf60a9
Kenta Yamada <51431248+kurikinton105@users.noreply.github.com>
2022-05-27 20:08:46 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is yama's "make something up project!!"
+        </p>
+        <a
+          className="App-link"
+          href="https://yama-code.tech/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          yama-code.tech
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
